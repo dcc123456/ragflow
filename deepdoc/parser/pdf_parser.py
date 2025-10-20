1f2f266efa2345468858c0973ebc8780dad2ab61
Kevin Hu <kevinhu.sh@gmail.com>
2025-10-20 15:19:48 +0800

    Fix: ocr client error.

--- deepdoc/parser/pdf_parser.py
+++ deepdoc/parser/pdf_parser.py
@@ -299,7 +299,7 @@ class RAGFlowPdfParser:
 
     def __ocr(self, pagenum, img, chars, ZM=3, device_id: int | None = None):
         start = timer()
-        bxs = self.ocr.detect(np.array(img), device_id)
+        bxs = self.ocr.detect(np.array(img), device_id=device_id)
         logging.info(f"__ocr detecting boxes of a image cost ({timer() - start}s)")
 
         start = timer()
@@ -352,7 +352,7 @@ class RAGFlowPdfParser:
                 b["box_image"] = self.ocr.get_rotate_crop_image(img_np, np.array([[left, top], [right, top], [right, bott], [left, bott]], dtype=np.float32))
                 boxes_to_reg.append(b)
             del b["txt"]
-        texts = self.ocr.recognize_batch([b["box_image"] for b in boxes_to_reg], device_id)
+        texts = self.ocr.recognize_batch([b["box_image"] for b in boxes_to_reg], device_id=device_id)
         for i in range(len(boxes_to_reg)):
             boxes_to_reg[i]["text"] = texts[i]
             del boxes_to_reg[i]["box_image"]
