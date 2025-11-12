09a7db22cf585686436f20bfa5693b5443487d90
Kevin Hu <kevinhu.sh@gmail.com>
2025-11-12 19:49:46 +0800

    Fix: raptor & graphrag enqueue.

--- api/db/services/document_service.py
+++ api/db/services/document_service.py
@@ -870,7 +870,10 @@ def queue_raptor_o_graphrag_tasks(sample_doc_id, ty, priority, fake_doc_id="", d
     task["doc_id"] = fake_doc_id
     task["doc_ids"] = doc_ids
     DocumentService.begin2parse(sample_doc_id["id"])
-    assert REDIS_CONN.queue_product(settings.get_svr_queue_name(priority), message=task), "Can't access Redis. Please check the Redis' status."
+
+    assert RABBITMQ_CONN.queue_product(
+        rout_key(priority, ty), message=task
+    ), "Can't access Redis. Please check the Redis' status."
     return task["id"]
 
 
