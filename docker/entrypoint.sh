4ff34c9144bf6daa4c5b30874b309eabc11c0309
asiroliu <asiro@qq.com>
2025-11-24 19:58:52 +0800

    ci: Restructure test workflow conditions and add RabbitMQ port configurations
    - Modify job execution conditions to run only on pushes with commit messages or scheduled runs
    - Remove NEED_MIRROR build argument from Docker build
    - Add RabbitMQ port configurations with dynamic port allocation
    - Fix RabbitMQ port assignments to use proper variables
    - Remove several test steps (SDK, frontend API, and HTTP API tests)
    - Remove container stop step

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,18 @@
 
 /usr/sbin/nginx
 
+# -----------------------------------------------------------------------------
+# Replace env variables in the service_conf.yaml file
+# -----------------------------------------------------------------------------
+CONF_DIR="/ragflow/conf"
+TEMPLATE_FILE="${CONF_DIR}/service_conf.yaml.template"
+CONF_FILE="${CONF_DIR}/service_conf.yaml"
+
+rm -f "${CONF_FILE}"
+while IFS= read -r line || [[ -n "$line" ]]; do
+    eval "echo \"$line\"" >> "${CONF_FILE}"
+done < "${TEMPLATE_FILE}"
+
 export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu/
 
 PY=/root/miniconda3/envs/py11/bin/python
