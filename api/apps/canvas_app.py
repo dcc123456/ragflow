dc408305e947751c52bc1baa49704e104778388e
Kevin Hu <kevinhu.sh@gmail.com>
2025-12-22 09:50:32 +0800

    Fix: Pipleline debug error./ (#51)

--- api/apps/canvas_app.py
+++ api/apps/canvas_app.py
@@ -156,7 +156,7 @@ async def run():
     if cvs.canvas_category == CanvasCategory.DataFlow:
         task_id = get_uuid()
         Pipeline(cvs.dsl, tenant_id=current_user.id, doc_id=CANVAS_DEBUG_DOC_ID, task_id=task_id, flow_id=req["id"])
-        ok, error_message = await asyncio.to_thread(queue_dataflow, user_id, req["id"], task_id, files[0], 0)
+        ok, error_message = await asyncio.to_thread(queue_dataflow, user_id, req["id"], task_id, file=files[0], priority=0)
         if not ok:
             return get_data_error_result(message=error_message)
         return get_json_result(data={"message_id": task_id})
