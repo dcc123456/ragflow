a1e215a60a3a073dd5b9be63eb518ba869d907d4
balibabu <cike8899@users.noreply.github.com>
2026-02-26 10:02:04 +0800

    Fix: Unable to be redirected to the homepage after logging in via third-party login (#196)

--- web/src/layouts/next.tsx
+++ web/src/layouts/next.tsx
@@ -1,3 +1,5 @@
+import { useAuth } from '@/hooks/auth-hooks';
+import { redirectToLogin, redirectToSpecifiedPage } from '@/utils/private-util';
 import React, { useEffect, useRef } from 'react';
 import { Outlet } from 'react-router';
 import { Header } from './next-header';
@@ -15,10 +17,26 @@ export default function NextLayout() {
     };
   }, []);
 
-  return (
-    <main ref={containerRef} className="h-full flex flex-col">
-      <Header></Header>
-      <Outlet />
-    </main>
-  );
+  const renderMain = () => {
+    return (
+      <main ref={containerRef} className="h-full flex flex-col">
+        <Header></Header>
+        <Outlet />
+      </main>
+    );
+  };
+
+  const { isLogin, redirectUrl } = useAuth();
+
+  if (isLogin === true) {
+    if (redirectUrl) {
+      redirectToSpecifiedPage(redirectUrl);
+    }
+
+    return renderMain();
+  } else if (isLogin === false) {
+    redirectToLogin();
+  }
+
+  return renderMain();
 }
