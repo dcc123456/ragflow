900c99df4fcbadc6dee33454fa56c6f5633849db
balibabu <cike8899@users.noreply.github.com>
2026-03-18 14:52:21 +0800

    Fix: Unable to invite other users. (#219)

--- web/src/layouts/components/bell-button.tsx
+++ web/src/layouts/components/bell-button.tsx
@@ -1,18 +1,13 @@
 import { Button } from '@/components/ui/button';
-import { Routes } from '@/routes';
 import { BellRing } from 'lucide-react';
+import { TeamInvitationReminderDialog } from './team-invitation-reminder-dialog';
 
 export function BellButton() {
   return (
-    <Button
-      asLink
-      to={`${Routes.UserSetting}${Routes.Team}`}
-      variant="ghost"
-      size="icon"
-      className="group"
-      dot
-    >
-      <BellRing className="size-4 animate-bell-shake group-hover:animate-none" />
-    </Button>
+    <TeamInvitationReminderDialog>
+      <Button variant="ghost" size="icon" className="group" dot>
+        <BellRing className="size-4 animate-bell-shake group-hover:animate-none" />
+      </Button>
+    </TeamInvitationReminderDialog>
   );
 }
