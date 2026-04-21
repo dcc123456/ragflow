d3775445f2caa8baf54f5f621b1b361074c308f4
bill <yibie_jingnian@163.com>
2026-04-21 17:17:11 +0800

    Feat: Send a heartbeat request every two minutes.

--- web/src/layouts/root-layout.tsx
+++ web/src/layouts/root-layout.tsx
@@ -1,3 +1,4 @@
+import { useHeartBeat } from '@/hooks/use-heart-beat-request';
 import React from 'react';
 import { Outlet } from 'react-router';
 import { Header } from './components/header';
@@ -5,6 +6,8 @@ import { Header } from './components/header';
 export const nextLayoutRef = React.createRef<HTMLDivElement>();
 
 export function RootLayoutContainer({ children }: React.PropsWithChildren) {
+  useHeartBeat();
+
   return (
     <div className="size-full grid grid-rows-[auto_1fr] grid-cols-1 grid-flow-col">
       <Header className="px-5 py-4" />
