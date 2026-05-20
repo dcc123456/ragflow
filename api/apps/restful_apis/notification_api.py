f0726d02f5e1ca19fc595e038edb866c0a21f484
Lynn <lynn_inf@hotmail.com>
2026-05-20 16:38:24 +0800

    feat: notification (#488)

---
+++ api/apps/restful_apis/notification_api.py
@@ -0,0 +1,19 @@
+from api.db.services.system_settings_service import SystemSettingsService
+from api.utils.api_utils import get_json_result
+from api.utils.system_settings_utils import load_value_from_string
+
+NOTIFICATION_FIELDS = {
+    "notification.content": "content",
+    "notification.enabled": "enabled"
+}
+
+
+@manager.route("/notification", methods=["GET"])  # noqa: F821
+def get_notification():
+    notification_rows = SystemSettingsService.get_by_name("notification.")
+    result = {}
+    for row in notification_rows:
+        key = NOTIFICATION_FIELDS.get(row.name)
+        if key:
+            result[key] = load_value_from_string(row.value, row.data_type)
+    return get_json_result(data=result)
